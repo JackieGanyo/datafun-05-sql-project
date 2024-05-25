614e0f0bee97550f83b87d57102e410fdeef61cb
JackieGanyo <jacquelynganyo@gmail.com>
2024-05-24 21:04:35 -0500

    Initial commit for setup of the project. · 1e3b3b7d

--- db_operations_Ganyo.py
+++ db_operations_Ganyo.py
@@ -1,1 +1,16 @@
+
+'''
 Operations Script
+This script contains functions for performing database operations in the datafun-05-sql-project.
+It requires the following imports:
+import sqlite3
+import pandas as pd
+import datetime
+
+'''
+# Import Standard Libraries
+
+#Import External Libraries
+import sqlite3
+import pandas as pd
+import datetime
