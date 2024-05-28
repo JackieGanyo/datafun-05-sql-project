c6870629bbc0827d5d852862561be716e6454ff7
JackieGanyo <jacquelynganyo@gmail.com>
2024-05-27 19:53:27 -0500

    Database creation & file organization

--- db_operations_Ganyo.py
+++ db_operations_Ganyo.py
@@ -17,3 +17,17 @@ import sqlite3
 import pandas as pd
 import pyarrow as pa
 import datetime
+'''Logging Configuration for the project
+logging.basicConfig(
+    filename='log.txt',
+    level=logging.DEBUG, 
+    filemode='a',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+ # Example usage:
+logging.debug("This is a debug message.")
+logging.info("This is an info message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.")
+'''
