06b8271e77dd875cae7f176021492842a87be36f
JackieGanyo <jacquelynganyo@gmail.com>
2024-05-29 23:27:53 -0500

    Filter last by "s"

--- sql/query_filter.sql
+++ sql/query_filter.sql
@@ -1,7 +1,7 @@
 --Query filter tables using WHERE and conditions
 
 --Filter authors by last name beginning with 'S'
-SELECT * FROM authors WHERE last = 'S*';
+SELECT * FROM authors WHERE last LIKE 'S%';
 
 --Filter books by year published in 1900s
 SELECT * FROM books WHERE year_published BETWEEN 1900 AND 1999; 
